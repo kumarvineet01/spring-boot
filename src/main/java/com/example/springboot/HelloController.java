15cc6202dadc2ad71507896ba3806b9a5fe09175
kumarvineet01 <kumarvineet01@gmail.com>
2022-03-30 18:12:49 +0530

    HelloControler.java Changes

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Azure DevOps Deplyoment . . . . !";
+		return "Azure DevOps Deplyoment on Webaap . . . . !";
 	}
 
 }
